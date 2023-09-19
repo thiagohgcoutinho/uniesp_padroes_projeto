82abec2803cc0c53b146edbe551d961b914f4d0d
Thiago Coutinho <2022211510098@iesp.edu.br>
2023-09-19 16:20:42 -0300

    aula

--- Aula_03/src/tech/thiagohgcoutinho/Bee/Main.java
+++ Aula_03/src/tech/thiagohgcoutinho/Bee/Main.java
@@ -4,19 +4,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException{
+
+        int X;
         Scanner input = new Scanner(System.in);
-
-        int A = input.nextInt();
-        int B = input.nextInt();
-        int C = input.nextInt();
-        int D = input.nextInt();
-
-
-        if ((B > C) && (D > A) && ((C + D) > (A + B)) && (C > 0) && (D > 0) && ((A % 2) == 0)){
-            System.out.println("Valores aceitos");
-        } else {
-            System.out.println("Valores nao aceitos");
+        X = input.nextInt();
+        int i =1;
+        while (i <= X){
+            if (i % 2 != 0){
+                System.out.println(i);
+            }
+            i++;
         }
+
+
+
+
+
+
+        double valor;
+
+        valor = input.nextDouble();
+
+        if ( valor >= 0 && valor <= 25) {
+            System.out.println("Intervalo [0,25]");
+        } else if (valor > 25 && valor <= 50) {
+            System.out.println("Intervalo (25,50]");
+        } else if (valor > 50 && valor <= 75) {
+            System.out.println("Intervalo (50,75]");
+        } else if (valor > 75 && valor <=100) {
+            System.out.println("Intervalo (75,100]");
+        } else {
+            System.out.println("Fora de intervalo");
+        }
+
+
     }
 }
 
